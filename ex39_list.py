9fd6302cb216f7c980481c2b4a498a37b5ba3a5c
Luo Mingmao <luomingmao@gmail.com>
2017-04-26 00:07:33 +0800

    Updated at 2017/04/26

--- ex39_list.py
+++ ex39_list.py
@@ -1,6 +1,6 @@
 ten_things = "Apples oranges Crows Telephone Light Sugar"
 
-print "Wait there's not 10 things in htat list, let's fix that."
+print "Wait there's not 10 things in that list, let's fix that."
 
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
